a33eded098e78e96244575cf60afc9d948ca4b70
Jan Sobeslav <sobeslav.jan@seznam.cz>
2021-11-03 18:16:09 +0100

    first commit

---
+++ js/data.js
@@ -0,0 +1,41 @@
+let data = [
+    {
+        name: "Hrad",
+        active: "false",
+        level: 1,
+        priceGold: 15,
+        priceCoins: 1,
+        time: 10, //ČAS ZJISTIT
+        icon: "fab fa-fort-awesome-alt"
+    },
+    {
+        name: "Zlatý důl",
+        active: "false",
+        level: 1,
+        priceGold: 20,
+        priceCoins: 2,
+        time: 10, //ČAS ZJISTIT
+        icon: "fas fa-cubes"
+    },
+    {
+        name: "Kasárna",
+        active: "false",
+        level: 0,
+        priceGold: 10,
+        priceCoins: 1,
+        time: 10, //ČAS ZJISTIT
+        icon: "fas fa-chess-rook"
+    },
+    {
+        name: "Mincovna",
+        active: "false",
+        level: 0,
+        priceGold: 28,
+        priceCoins: 3,
+        time: 10, //ČAS ZJISTIT
+        icon: "fas fa-coins"
+    }
+];
+let settings = {
+    activeLink: "index",
+};

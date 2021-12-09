b0422241f35e8614eb47b93bc606dd1f402fb3bc
JanSobeslav <sobeslav.jan@seznam.cz>
2021-12-09 21:14:02 +0100

    castle comp, render on click on sidebar item

---
+++ js/castle.js
@@ -0,0 +1,152 @@
+import { createElement, getElement } from "./elements.js";
+
+class Model {
+    constructor(data) {
+        this.allData = data;
+        this.data = data.data;
+        this.settings = data.settings;
+    }
+
+    bindDisplayBuildings(callback) {
+        this.onDisplayBuildings(callback);
+    }
+}
+
+class View {
+    constructor(elName) {
+        this.app = getElement(elName);
+
+        this.container = createElement('div', ['container-fluid', 'px-4']);
+
+        this.h1 = createElement('h1', ['mt-4']);
+
+        //NEFUNKČNÍ
+        this.infoButton = createElement('button');
+        this.infoButton.setAttribute('type', 'button');
+        this.infoButton.setAttribute('data-bs-container', 'body');
+        this.infoButton.setAttribute('data-bs-toggle', 'popover');
+        this.infoButton.setAttribute('data-bs-placement', 'right');
+        this.infoButton.setAttribute('style', 'background-color: transparent; border: solid transparent;');
+        this.infoButton.setAttribute('data-bs-content', ('Hrad je místo, odkud řídíš své město. Především tu stavíš a vylepšuješ' +
+            'budovy, ale také máš možnost zde vidět svůj postup. Kolik si rekrutoval jednotek, kolik stupňů budov postavil,' +
+            'kolik zlata jsi vytěžil, nebo kolik mincí si vyrazil.'));
+        this.infoButton.innerHTML = '<i class="fas fa-info-circle"></i>';
+
+        this.nav = createElement('nav');
+
+        this.navTabs = createElement('div', ['nav', 'nav-tabs']);
+
+        this.navTabsBtnBuild = createElement('button', ['nav-link', 'active']);
+        this.navTabsBtnBuild.id = 'nav-buildings-tab';
+        this.navTabsBtnBuild.textContent = 'Budovy';
+        this.navTabsBtnBuild.setAttribute('data-bs-toggle', 'tab');
+        this.navTabsBtnBuild.setAttribute('data-bs-target', '#nav-buildings');
+        this.navTabsBtnBuild.setAttribute('type', 'button');
+        this.navTabsBtnBuild.setAttribute('role', 'tab');
+        this.navTabsBtnBuild.setAttribute('aria-controls', 'nav-buildings');
+        this.navTabsBtnBuild.setAttribute('aria-selected', 'true');
+
+        this.navTabsBtnProgress = createElement('button', ['nav-link']);
+        this.navTabsBtnProgress.id = 'nav-progress-tab';
+        this.navTabsBtnProgress.textContent = 'Postup';
+        this.navTabsBtnProgress.setAttribute('data-bs-toggle', 'tab');
+        this.navTabsBtnProgress.setAttribute('data-bs-target', '#nav-progress');
+        this.navTabsBtnProgress.setAttribute('type', 'button');
+        this.navTabsBtnProgress.setAttribute('role', 'tab');
+        this.navTabsBtnProgress.setAttribute('aria-controls', 'nav-progress');
+        this.navTabsBtnProgress.setAttribute('aria-selected', 'true');
+
+        this.navTabs.append(this.navTabsBtnBuild, this.navTabsBtnProgress);
+
+        this.nav.append(this.navTabs);
+
+        this.tabContent = createElement('div', ['tab-content']);
+        this.tabContent.id = 'nav-tabContent';
+
+        this.tabPane = createElement('div', ['tab-pane', 'fade', 'show', 'active']);
+        this.tabPane.id = 'nav-buildings';
+        this.tabPane.role = 'tabpanel';
+        this.tabPane.setAttribute('aria-labelledby', 'nav-buildings-tab');
+
+        this.tabPaneContBuildings = createElement('div', ['container', 'mt-4', 'ml-4']);
+
+        //generuje se v displayBuildings
+        
+        this.tabPane.append(this.tabPaneContBuildings);
+
+        this.tabPane2 = createElement('div', ['tab-pane', 'fade', 'show']);
+        this.tabPane2.id = 'nav-progress';
+        this.tabPane2.role = 'tabpanel';
+        this.tabPane2.setAttribute('aria-labelledby', 'nav-progress-tab');
+        this.tabPane2.textContent = '--- Coming Soon! ---';
+
+        this.tabContent.append(this.tabPane, this.tabPane2);
+
+        this.container.append(this.h1, this.infoButton, this.nav, this.tabContent);
+
+        this.app.append(this.container);
+    }
+
+    displayBuildings(data) {
+        let tpcRowHeader = createElement('div', ['row', 'justify-content-around', 'mb-4']);
+        tpcRowHeader.innerHTML = `
+        <div class="col-2">
+            <b>Budova</b>
+        </div>
+        <div class="col-2">
+            <b>Stupeň budovy</b>
+        </div>
+        <div class="col-2">
+            <b>Cena dalšího stupně</b>
+        </div>
+        <div class="col-2">
+            <b>Čas pro stavbu</b>
+        </div>
+        <div class="col-2">
+            <b>Vylepšit</b>
+        </div>
+        `;
+        this.tabPaneContBuildings.append(tpcRowHeader);
+        for (let budova of data) {
+            let row = createElement('div', ['row', 'justify-content-around', 'mb-2']);
+            row.innerHTML = `
+            <div class="col-2">
+                <i class="${budova.icon}"></i> ${budova.name}
+            </div>
+            <div class="col-2">
+                ${budova.level}
+            </div>
+            <div class="col-2">
+                ${budova.priceGold} <i class="fas fa-cube" style="color: rgb(139, 126, 0);"></i> /
+                ${budova.priceCoins} <i class="fas fa-circle" style="color: rgb(139, 126, 0);"></i>
+            </div>
+            <div class="col-2">
+                ${budova.time}
+            </div>
+            <div class="col-2">
+                <button class="btn btn-primary">Level 2</button>
+            </div>
+            `;
+            //@ToDo - zprovoznit button
+            this.tabPaneContBuildings.append(row);
+        }
+
+    }
+}
+
+class Controller {
+    constructor(model, view) {
+        this.model = model
+        this.view = view
+
+        this.onDisplayBuildings(this.model.data);
+    }
+
+    onDisplayBuildings = (data) => {
+        this.view.displayBuildings(data);
+    }
+}
+
+export function buildCastle(elName, data) {
+    const app = new Controller(new Model(data), new View(elName));
+}
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,19 +1,21 @@
 import { createElement, getElement } from "./elements.js";
+import { getData } from "./data.js";
+import { buildCastle as castle } from "./castle.js";
 
 class Model {
     constructor(data) {
         this.allData = data;
         this.data = data.data;
         this.settings = data.settings;
-     }
+    }
 
-     bindMenuItemsChange(callback) {
-         this.onMenuItemsChange = callback;
-     }
+    bindMenuItemsChange(callback) {
+        this.onMenuItemsChange = callback;
+    }
 
-     linkChange(link) {
-         this.settings.activeLink = link;
-     }
+    linkChange(link) {
+        this.settings.activeLink = link;
+    }
 }
 
 class View {
@@ -22,68 +24,84 @@ class View {
         //root of sidebar
         this.sidenav = createElement('div');
         this.sidenav.id = 'layoutSidenav';
-            this.side_nav = createElement('div');
-            this.side_nav.id = 'layoutSidenav_nav';
-            //nav
-                this.nav = createElement('nav', ['sb-sidenav', 'accordion', 'sb-sidenav-dark']);
-                this.nav.id = 'sidenavAccordion';
-                    //nav-menu
-                    this.navMenu = createElement('div', ['sb-sidenav-menu']);
-                        //menu
-                        this.menu = createElement('div', ['nav']);
-                            //menu items
-                            //### NAČÍST MENU
-                    this.navMenu.append(this.menu);
-                this.nav.append(this.navMenu);
-            this.side_nav.append(this.nav);
-        this.sidenav.append(this.side_nav);
+        this.side_nav = createElement('div');
+        this.side_nav.id = 'layoutSidenav_nav';
+        //nav
+        this.nav = createElement('nav', ['sb-sidenav', 'accordion', 'sb-sidenav-dark']);
+        this.nav.id = 'sidenavAccordion';
+        //nav-menu
+        this.navMenu = createElement('div', ['sb-sidenav-menu']);
+        //menu
+        this.menu = createElement('div', ['nav']);
+        //menu items
+        //### NAČÍST MENU
+        this.navMenu.append(this.menu);
+        this.nav.append(this.navMenu);
+        this.side_nav.append(this.nav);
+
+        this.side_content = createElement('div');
+        this.side_content.id = 'layoutSidenav_content';
+        this.contentMain = createElement('main');
+        this.contentMain.id = 'content';
+        this.side_content.append(this.contentMain);
+        this.sidenav.append(this.side_nav, this.side_content);
         //
 
         this.app.append(this.sidenav);
 
         this._temporaryItemsLinks = [];
-     }
+    }
 
-     _initItemsLink(link) {
-         this._temporaryItemsLinks.push(link);
-     }
+    _initItemsLink(link) {
+        this._temporaryItemsLinks.push(link);
+    }
 
-     displaySidenavItems(data, settings) {
-         const menuHeading = createElement('div', ['sb-sidenav-menu-heading']);
-         menuHeading.textContent = 'Budovy';
-         this.menu.append(menuHeading);
-         for (const d of data) {
-             const a = createElement('a', ['nav-link', d.link]);
-             a.href = '#/' + d.link;
-             if (settings.activeLink === d.link) {
-                 a.classList.add('active');
-             }
-             a.innerHTML = '<div class="sb-nav-link-icon"><i class="' + d.icon + '"></i></div>' + d.name; 
+    displaySidenavItems(data, settings) {
+        const menuHeading = createElement('div', ['sb-sidenav-menu-heading']);
+        menuHeading.textContent = 'Budovy';
+        this.menu.append(menuHeading);
+        for (const d of data) {
+            const a = createElement('a', ['nav-link', d.link]);
+            a.href = '#/' + d.link;
+            if (settings.activeLink === d.link) {
+                a.classList.add('active');
+            }
+            a.innerHTML = '<div class="sb-nav-link-icon"><i class="' + d.icon + '"></i></div>' + d.name;
             this.menu.append(a);
             this._initItemsLink(d.link);
-         }
-     }
-
-     bindLinkChange(handler) {
-        for (let link of this._temporaryItemsLinks) {
-            let a = getElement('.' + link);
-            a.addEventListener('click', event => {
-            let aLinks = document.getElementsByTagName('a');
-            for (let aL of aLinks) {
-                aL.classList.remove("active");
-            } 
-            if (event.target.className === ('nav-link ' + link)) {
-                //this.menu.innerHTML = '';
-                a.classList.add("active");
-                console.log(a);
-                handler(link);
-            } else {
-                a.classList.remove("active");
-            }
-          });
         }
-        
-      }
+    }
+
+    bindLinkChange(handler) {
+        //projde linky v _temporaryItemsLinks
+        for (let link of this._temporaryItemsLinks) {
+            //najde konkrétní link s třídou
+            let a = getElement('.' + link);
+            //přidá event click na tento el
+            a.addEventListener('click', event => {
+                //nalezne všechny a elementy
+                let aLinks = document.getElementsByTagName('a');
+                //odstraní třídu active
+                for (let aL of aLinks) {
+                    aL.classList.remove("active");
+                }
+                //pokud se target rovná konkrétní třídě
+                if (event.target.className === ('nav-link ' + link)) {
+                    //najde element s id content, odstraní z něj veškeré children a vloží konrétní komponentu
+                    let content = getElement('#content');
+                    if (content.childNodes.length > 0) {
+                        content.removeChild(content.firstChild);
+                    }
+                    a.classList.add("active");
+                    handler(link);
+                    castle(('#' + content.id), getData());
+                } else {
+                    a.classList.remove("active");
+                }
+            });
+        }
+
+    }
 }
 
 class Controller {
@@ -102,7 +120,6 @@ class Controller {
 
     handleLinkChange = (link) => {
         this.model.linkChange(link);
-        //this.onMenuItemsChange(this.model.data, this.model.settings);
     }
 }
 
